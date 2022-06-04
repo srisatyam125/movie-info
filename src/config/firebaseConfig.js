ae23387a3f4fbd174e0c3616c18006e948b74d4e
srisatyam125 <srisatyam0526@gmail.com>
2022-06-04 21:28:43 +0530

    Action and Database connected.

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,7 @@
+export default {
+  apiKey: "YOUR_APIKEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  databaseURL: "YOUR_DATABASE_URL",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID"
+};
---
+++ src/config/theMovieDBConfig.js
@@ -0,0 +1,2 @@
+export const apiKey = '1b84556ac166c16f4df52fd0bdd3c565';
+
